2d76d6fa01e51a332c8852e2ca9968b38b702e61
Mike Bowman <mbowman2@gmail.com>
2016-02-03 15:45:12 -0500

    Add #show, #new, #create actions to reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,20 @@
 class ReservationsController < ApplicationController
   def show
+    @reservations = Reservation.all
   end
 
   def new
+    @reservation = Reservation.new
   end
 
   def create
+    @restaurant = Restaurant.find(params[:id])
+    @reservation = @restaurant.reservations.build(reservation_params)
+
+    if @reservation.save
+      redirect_to restaurant_path(@restaurant), notice: "Reservations created successfully!"
+    else
+      render restaurant_path(@restaurant)
+    end
   end
 end
