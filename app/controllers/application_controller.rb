c9da3e2307f3bc48095788446a1ce4dee44453ee
Mike Bowman <mbowman2@gmail.com>
2016-02-04 14:26:16 -0500

    Fix typo in helper_method#current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,5 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper method :current_user
+  helper_method :current_user
 end
