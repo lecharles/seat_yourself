fb573d2f0f8970a6ea2717255904774347ae6c46
Mike Bowman <mbowman2@gmail.com>
2016-02-05 16:48:45 -0500

    Run migration to add capacity to restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202211151) do
+ActiveRecord::Schema.define(version: 20160205214759) do
 
   create_table "reservations", force: :cascade do |t|
     t.datetime "time"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160202211151) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "capacity"
   end
 
   create_table "users", force: :cascade do |t|
