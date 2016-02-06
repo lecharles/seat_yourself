2f5e18a98191feb18578ddb00403fea777b897f5
Mike Bowman <mbowman2@gmail.com>
2016-02-06 14:36:27 -0500

    Try to calculate remaining seats based on capacity and party size

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,11 @@
 class Restaurant < ActiveRecord::Base
   has_many :reservations
   has_many :users, through: :reservations
+
+  # @capacity = self.capacity
+
+  def seats_remaining
+    @capacity
+  end
+
 end
