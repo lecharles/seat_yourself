709e794a0c629f46c9f47a745c1a29e2a213c291
steveoneil <steve@steveoneil.com>
2016-02-06 15:14:50 -0500

    Add owner id and association to restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :owner, class_name: 'User'
   has_many :reservations
   has_many :users, through: :reservations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :reservations
   has_many :restaurants, through: :reservations
+  has_many :owned_restaurants, class_name: 'Restaurant', foreign_key: "owner_id"
   validates :email, presence: true
   validates :password, presence: true
   validates :password_confirmation, confirmation: true
