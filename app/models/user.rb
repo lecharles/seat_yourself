219ef87cdbfde66835a631f7be163140d5c48eec
Mike Bowman <mbowman2@gmail.com>
2016-02-06 18:17:04 -0500

    Fix typo in user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :reservations
   has_many :restaurants, through: :reservations
   has_many :owned_restaurants, class_name: 'Restaurant', foreign_key: "owner_id"
-  validates :email, :first_name, :last_name presence: true
+  validates :email, :first_name, :last_name, presence: true
   validates :password, presence: true
   validates :password_confirmation, confirmation: true
 end
