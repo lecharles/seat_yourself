b249eba06e7da9a19cc7ee49f9b3dd27b26cab1f
Mike Bowman <mbowman2@gmail.com>
2016-02-08 13:23:38 -0500

    Clean up Restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,9 +2,4 @@ class Restaurant < ActiveRecord::Base
   belongs_to :owner, class_name: 'User'
   has_many :reservations, dependent: :destroy
   has_many :users, through: :reservations
-
-  def load_reservations_for_restaurant
-    Reservation.all.where(restaurant_id: self.id)
-  end
-
 end
