c6c2b6cbf906986d59b15d187ec8982d9d48cb04
steveoneil <steve@steveoneil.com>
2016-02-08 14:59:05 -0500

    Add route to login page after successful sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,6 @@ class ApplicationController < ActionController::Base
 
   private
   def current_user
-    puts session
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to restaurants_path, notice: "Signed up successfully!"
+      redirect_to new_session_path, notice: "Signed up successfully!"
     else render :new
     end
   end
